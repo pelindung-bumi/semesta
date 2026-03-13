5162f2dc0baeaf6a72c3e8e2117ffefb57770106
valdo766hi <rivaldosilalahi13@gmail.com>
2026-03-13 20:39:05 +0700

    chore: update the latest hardware-confoguration

--- hosts/vpn/hardware-configuration.nix
+++ hosts/vpn/hardware-configuration.nix
@@ -13,12 +13,23 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.ens3.useDHCP = lib.mkDefault true;
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/05d79746-f2f4-4c57-b8a5-0b13f75c5ad1";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/ff7a134f-be1e-41e3-ba1e-2fb547bac359";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot/efi" =
+    { device = "/dev/disk/by-uuid/0410-B659";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
