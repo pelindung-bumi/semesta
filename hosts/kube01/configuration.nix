c48b6415bcf92f0d34e34d54e7076176f4cb071f
valdo766hi <rivaldosilalahi13@gmail.com>
2026-03-21 21:38:30 +0700

    feat: add compatibility module for ceph

--- hosts/kube01/configuration.nix
+++ hosts/kube01/configuration.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ config, lib, ... }:
 {
   imports = [
     ../../modules/nixos/common.nix
@@ -9,6 +9,19 @@
   ] ++ lib.optional (builtins.pathExists ./hardware-configuration.nix) ./hardware-configuration.nix;
 
   networking.hostName = "kube01";
+
+  boot.kernelModules = [
+    "libceph"
+    "rbd"
+    "nbd"
+  ];
+
+  system.activationScripts.exposeKernelModules.text = ''
+    mkdir -p /lib/modules
+    ln -sfn \
+      "/run/current-system/kernel-modules/lib/modules/${config.boot.kernelPackages.kernel.modDirVersion}" \
+      "/lib/modules/${config.boot.kernelPackages.kernel.modDirVersion}"
+  '';
 
   boot.loader.efi.canTouchEfiVariables = false;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
