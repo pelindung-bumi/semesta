691c66d5a21d97e18b08bf5d85631054fc2439c3
Rivaldo Silalahi <57586418+valdo766hi@users.noreply.github.com>
2026-03-22 15:54:02 +0700

    chore: add proxy config to forward traffic to envoy gateway nodeport (#10)
    we expose service envoy gateway using nodeport, to expose it globally
    need to use external loadbalancer, we use nginx currently

--- hosts/lb01/nginx-lb.nix
+++ hosts/lb01/nginx-lb.nix
@@ -7,8 +7,16 @@
         listen 6443;
         proxy_pass 10.200.0.177:6443;
       }
+      server {
+        listen 80;
+        proxy_pass 10.200.0.177:30080;
+      }
+      server {
+        listen 443;
+        proxy_pass 10.200.0.177:30443;
+      }
     '';
   };
 
-  networking.firewall.allowedTCPPorts = [ 6443 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 6443 ];
 }
